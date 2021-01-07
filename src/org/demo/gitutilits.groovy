f852322b658794b87c0abec3429b378265f40699
harsharameshh <48444072+harsharameshh@users.noreply.github.com>
2021-01-07 12:58:56 +0530

    Update gitutilits.groovy

--- src/org/demo/gitutilits.groovy
+++ src/org/demo/gitutilits.groovy
@@ -1,1 +1,20 @@
 
+package org.demo
+
+def checkout(git_url,branch) {
+  git url: "git_url" , branch:"branch"
+}
+
+def intialize(nodeOs,repo)
+{
+  if(nodOs='windows')
+  {
+    bat 'git init'
+  }
+  else
+  {
+    sh 'git status'
+  }
+}
+
+    

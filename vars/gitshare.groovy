ac82457501316277691f6911556f5517532f9d41
harsharameshh <48444072+harsharameshh@users.noreply.github.com>
2021-01-07 14:08:30 +0530

    Update gitshare.groovy

--- vars/gitshare.groovy
+++ vars/gitshare.groovy
@@ -1,7 +1,6 @@
 #!/usr/bin/env groovy
 
 def call(String git_url,String gitbranch,String credentialsId,String stage ) {
-  echo stage;
   def myUtils = new org.demo.gitutilits()
    myUtils.checkout(git_url,gitbranch)
     

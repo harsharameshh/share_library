3ef02f51f3d5075b940dbf762d3ad1df466cd85d
harsharameshh <48444072+harsharameshh@users.noreply.github.com>
2021-01-08 10:44:23 +0530

    Update gitutilits.groovy

--- src/org/demo/gitutilits.groovy
+++ src/org/demo/gitutilits.groovy
@@ -2,7 +2,9 @@
 package org.demo
 
 def checkout(git_url,branch) {
-  git url: "$git_url" , branch:"$branch"
+  echo git_url;
+  echo branch;
+  //git url: "$git_url" , branch:"$branch"
   echo "harsha"
 }
 

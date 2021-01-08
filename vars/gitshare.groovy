e8c04fca26fa55ef08cd29554e53de8e2ba38ba4
harsharameshh <48444072+harsharameshh@users.noreply.github.com>
2021-01-08 12:07:52 +0530

    Update gitshare.groovy

--- vars/gitshare.groovy
+++ vars/gitshare.groovy
@@ -2,6 +2,7 @@
 
 def call(String git_url,String gitbranch,String credentialsId,String stage, String nodeOs ) {
   echo stage;
+  echo new_branch;
   def myUtils = new org.demo.gitutilits()
   // def cl"osure = myUtils.&"stage"
   myUtils."$stage"(nodeOs,git_url,gitbranch)
